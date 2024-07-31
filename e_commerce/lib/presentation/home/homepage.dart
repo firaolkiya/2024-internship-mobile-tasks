8508aac1b4710e32228676297406d3952eab0e5e
firaol <firaolkiya6@gmail.com>
2024-07-31 11:26:45 +0300

    updated

--- e_commerce/lib/presentation/home/homepage.dart
+++ e_commerce/lib/presentation/home/homepage.dart
@@ -14,6 +14,7 @@ class Homepage extends StatelessWidget {
         floatingActionButton: InkWell(
           onTap: (){Get.to(AddProduct());},
           child: const CircleAvatar(
+            radius: 25,
             child: Icon(Icons.add),
             
             backgroundColor: Colors.blue,
